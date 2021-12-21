0688c6211b9995aae747f980d84d061eb92166fa
hardikvasar1 <hardik.vasar@commercepundit.com>
2021-12-21 12:08:39 +0530

    2 file

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './style.css';
 
 export default function App() {
-  let name = 'hardik';
-  let age = 21;
+  const name = {
+    first_name: 'hardik',
+    last_name: 'vasar',
+  };
   return (
     <div>
       <h1>
-        Hello my name is {name} and my age is {age}
+        Hello my name is {name.first_name} and my age is {name.last_name}
       </h1>
     </div>
   );
