ca1889879b8723bba27b74796c94f4dbc3b8965e
hardikvasar1 <hardik.vasar@commercepundit.com>
2021-12-21 13:32:12 +0530

    1 file modified, 2 added

---
+++ src/demo9.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+export default function demo9() {
+  const MyFunction = () => {
+    return <h1>I Love React</h1>;
+  };
+  return (
+    <>
+      <MyFunction />
+    </>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import Demo7 from './demo7';
 import Demo8 from './demo8';
+import Demo9 from './demo9';
 
-ReactDOM.render(<Demo8 />, document.getElementById('root'));
+ReactDOM.render(<Demo9 />, document.getElementById('root'));
